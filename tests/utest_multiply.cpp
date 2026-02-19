ff0f06cb7f812191498e09fc7978e9a7eeccc2bd
udaym9651 <uday.m9651@gmail.com>
2026-02-19 12:46:59 +0530

    Update utest_multiply.cpp

--- tests/utest_multiply.cpp
+++ tests/utest_multiply.cpp
@@ -1,9 +1,18 @@
 #include <cassert>
 #include <iostream>
 #include "product.h"
-int main()
+
+
+namespace
 {
-Product p;
-assert(p.computeA(-10, 20) == -200);
-assert(p.computeB(-10, 20, 0) == -100);
+ TEST(PersonTestSuite, functionA)
+    {
+        Product p;
+        assert(p.computeA(-10, 20) == -200);
+    }
+TEST(PersonTestSuite, functionA)
+    {
+        Product p;
+        assert(p.computeB(-10, 20, 0) == -100);
+    }
 }
