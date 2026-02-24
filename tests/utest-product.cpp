3d6a109371268af2137896e5d90fc42d97249fdb
udaym9651 <uday.m9651@gmail.com>
2026-02-24 13:05:17 +0530

    Update utest-product.cpp

--- tests/utest-product.cpp
+++ tests/utest-product.cpp
@@ -5,25 +5,21 @@
 
 namespace
 {
-    TEST(PersonTestSuite, functionA)
-    {
+    TEST(PersonTestSuite, functionA){
         Product p;
         EXPECT_EQ(p.computeA(10, 20), 200);
     }
-    TEST(PersonTestSuite, functionB)
-    {
+    TEST(PersonTestSuite, functionB){
         Product p;
         EXPECT_EQ(p.computeB(10, 20, 30), 6000);
     }
 
-    TEST(PersonTestSuite, not_equal_functionB)
-    {
+    TEST(PersonTestSuite, not_equal_functionB){
         Product p;
         EXPECT_NE(p.computeB(10, 20, 30), 5000);
     }
 
-    TEST(PersonTestSuite, fun_B_greater_funA)
-    {
+    TEST(PersonTestSuite, fun_B_greater_funA){
         Product p;
         EXPECT_GT(p.computeB(10, 20, 30), p.computeA(10, 10));
     }
